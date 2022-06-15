bc0a8a45d4f6944da66b346b40b98bd15d42b15f
Tolga Torun <dtolgatorun@gmail.com>
2022-06-15 15:33:06 +0300

    blog post create,edit,update and index

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -29,4 +29,8 @@ def create_app(test_config=None):
 
     from . import auth
     app.register_blueprint(auth.bp)
+
+    from . import blog
+    app.register_blueprint(blog.bp)
+    app.add_url_rule('/', endpoint='index') #index view will be at "/" URL,url_for("index") and url_for("blog.index") both will generate "/" URL
     return app
